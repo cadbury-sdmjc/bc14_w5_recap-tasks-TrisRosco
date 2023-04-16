5ee123c3560577c234f6a5f8e8053b81be2d225c
trisrosco <trisrosco@gmail.com>
2023-04-16 19:49:32 +0100

    it's throwing up errors on submit, but it's probly because i've not done something yet

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CommentList from "../CommentList";
 import CommentForm from "../CommentForm";
+import Comment from "../Comment";
 import BlogPost from "../BlogPost";
 
 function App() {
