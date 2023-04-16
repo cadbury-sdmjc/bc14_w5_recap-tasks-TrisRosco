f5e0427770cdaf4c1ed07f5911b07895a19d8cef
trisrosco <trisrosco@gmail.com>
2023-04-16 19:02:10 +0100

    task 1 and maybe 2?

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,5 +1,31 @@
+import React from "react";
+
+//a function that returns a blog post using an object called blog
+
 function BlogPost() {
-  return null;
+  const blog = {
+    title: "My First Blog Post (wow!)",
+    author: "Reginald M. Javascpriptte III",
+    datePosted: "January 1, 2020",
+    content: `A structure used in most apps and games.
+    It's a way to keep doing the same.
+    While a condition is true,
+    Or for one to twenty-two.
+    If endless, for errors we blame
+    ..........`,
+    imageSrc: `https://i.pinimg.com/originals/4c/a3/1e/4ca31e5343e89bc550044caec29543e6.jpg`,
+    imageAlt: `A picture of Reginal Javascpriptte III`,
+  };
+
+  return (
+    <div>
+      <h1>{blog.title}</h1>
+      <h2>By {blog.author}</h2>
+      <h3>Posted on {blog.datePosted}</h3>
+      <img src={blog.imageSrc} alt={blog.imageAlt} />
+      <p>{blog.content}</p>
+    </div>
+  );
 }
 
 export default BlogPost;
--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,5 +1,21 @@
-function Comment() {
-  return null;
+function Comment(props) {
+  const { author, content } = props;
+
+  const initials = author
+    .split(" ")
+    .map((name) => name[0])
+    .join("")
+    .toUpperCase();
+
+  return (
+    <div>
+      <h3>{author}</h3>
+      <p>{content}</p>
+      <div>{initials}</div>
+    </div>
+  );
 }
+
+// return null;
 
 export default Comment;
