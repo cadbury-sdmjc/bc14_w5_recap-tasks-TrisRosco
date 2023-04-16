48ea289a99e91e893b72e67a37490c1d7a5f18d3
trisrosco <trisrosco@gmail.com>
2023-04-16 19:06:55 +0100

    task 3? I have no idea if any of this works

--- src/components/CommentList/index.js
+++ src/components/CommentList/index.js
@@ -1,5 +1,29 @@
 function CommentList() {
-  return null;
+  const comments = [
+    {
+      id: "kskBC5HZ8qgNQUiW6If6q",
+      asuthor: "Wilbur Wetblankette",
+      content: "Not your best work, Reginald.",
+    },
+
+    {
+      id: "jFyGAKz1VsGputO1gV8xa",
+      auther: "Angela Wilkes",
+      content: "I'm not sure about this one, Reggie.",
+    },
+  ];
+
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Comment
+          key={comment.id}
+          author={comment.author}
+          content={comment.content}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default CommentList;
